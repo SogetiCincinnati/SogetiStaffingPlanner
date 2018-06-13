bc8fae12bfc928e398ad92873e456fdaa5f5d67d
Alexander Luken <Alexander.Luken@us.sogeti.com>
2018-06-13 13:39:08 -0400

    10-B-Data Access Dashboard object, created model of DashboardData called MainViewData.  Created function in HomeController that returns a json message of the procedure data.

--- SogetiStaffingPlanner/Controllers/HomeController.cs
+++ SogetiStaffingPlanner/Controllers/HomeController.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SogetiStaffingPlanner.Models;
 
 namespace SogetiStaffingPlanner.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
+        public ActionResult Index()
 		{
 			return View();
 		}
@@ -26,5 +28,39 @@ namespace SogetiStaffingPlanner.Controllers
 
 			return View();
 		}
+        /*
+         * Method for getting the data for the main view
+         */
+        [HttpGet]
+        public JsonResult GetMainData()
+        {
+            //create the object to connect to the database
+            Dev_ClientOpportunitiesEntities item = new Dev_ClientOpportunitiesEntities();
+            //get the results
+            ObjectResult<MainView_Result> results =item.MainView();
+            var returner = new List<MainView_Result> { };
+            //map it to a json object
+            foreach (MainView_Result mvR in results)
+            {
+                returner.Add(new MainView_Result
+                {
+                    OpportunityName = mvR.OpportunityName,
+                    AE = mvR.AE,
+                    Active = mvR.Active,
+                    ClientContact = mvR.ClientContact,
+                    ClientName = mvR.ClientName,
+                    ClientSubbusiness = mvR.ClientSubbusiness,
+                    ConsultantGradeName = mvR.ConsultantGradeName,
+                    ExpectedStartDate = mvR.ExpectedStartDate,
+                    LastModified = mvR.LastModified,
+                    NumberOfPositions = mvR.NumberOfPositions,
+                    PracticeName = mvR.PracticeName,
+                    ProposedCandidate = mvR.ProposedCandidate,
+                    Rate = mvR.Rate,
+                    Skillset = mvR.Skillset
+                });
+            }
+            return Json(returner, JsonRequestBehavior.AllowGet);
+        }
 	}
 }

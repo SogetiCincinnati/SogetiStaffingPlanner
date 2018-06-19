d1ed9e9f7e1b64f99a0033a9e3b1536487eef488
Zoe Baker <Zoe.Baker@us.sogeti.com>
2018-06-19 12:12:36 -0400

    25-E-Add Client-Add Client Data Access- Removed unneeded views, renamed and corrected AddClient method to allow HttpPost calls and some error checking.

--- SogetiStaffingPlanner/Controllers/ClientController.cs
+++ SogetiStaffingPlanner/Controllers/ClientController.cs
@@ -9,79 +9,89 @@ namespace SogetiStaffingPlanner.Controllers
 {
     public class ClientController : Controller
     {
-        //  private Dev_ClientOpportunitesEntites dbo = new ClientOpportunitesEntites();
-
         ClientOpportunitiesEntities db = new ClientOpportunitiesEntities();
 
-        //GET ALL CLIENTS INFO
-      //  [HttpGet]
-     /*   public ActionResult Index()
-        {
-
-            System.Diagnostics.Debug.WriteLine("CLIENT INDEX FUNCTION EXECUTED!!!!!!!!!@@@@@@");
-
-
-            var clientDetails1 = from s in db.Clients
-                                 select new Client
-                                 {
-                                     ClientId = s.ClientId,
-                                     ClientName = s.ClientName,
-                                     ClientSubbusiness = s.ClientSubbusiness,
-                                     LastModifiedUserId = s.LastModifiedUserId,
-                                     LastModified = s.LastModified,
-                                     Active = s.Active,
-
-                                 };
-            return Json(clientDetails1, JsonRequestBehavior.AllowGet);
-
-        } */
-
-
-
-
-        // CLIENT INFO BY ID
-        [HttpGet]
-        public ActionResult ClientDetails(int? id)
-        {
+		/*
+		* POST: /Client/AddClient
+		* Adds a client into the entity framwork using a Post call. Last Modified Date and Active are set in this method.
+		*/
+		[HttpPost]
+		public ActionResult AddClient(int clientId, string clientName, string clientSubbusiness, int? lastModifiedUserId, DateTime? lastModified, bool? active)
+		{
+			try
+			{
+				Client client = new Client()
+				{
+					ClientId = clientId,
+					ClientName = clientName,
+					ClientSubbusiness = clientSubbusiness,
+					LastModifiedUserId = lastModifiedUserId,
+					LastModified = DateTime.Now,
+					Active = true
+				};
+				db.Clients.Add(client);
+				db.SaveChanges();
+			}
+			catch (Exception e)
+			{
+				System.Diagnostics.Debug.WriteLine(e.ToString());
+				return Json("Client Add Failed", JsonRequestBehavior.AllowGet);
+			}
+			return Json("Client Added Successfully", JsonRequestBehavior.AllowGet);
+		}
 
 
+		//GET ALL CLIENTS INFO
+		//  [HttpGet]
+		/*   public ActionResult Index()
+		   {
 
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
-            }
-            Client client = db.Clients.Find(id);
-            if (client == null)
-            {
-                return HttpNotFound();
-            }
-            return Json(client, JsonRequestBehavior.AllowGet);
-        }
-       
-      /*  public ActionResult Create()
-        {
-            return View();
-        } */
-
-        [HttpPost]
-        public ActionResult Create([Bind(Include = "ClientId, ClientName, ClientSubbusiness, LastModifiedUserId,LastModified, Active")] Client client) {
-
-            if (ModelState.IsValid) {
-
-                db.Clients.Add(client);
-              //  db.CLIENT_DETAILS.Add(client);
-                client.LastModified = DateTime.Now;
-                db.SaveChanges();
-            }
-            return Json("Client Added Successfully", JsonRequestBehavior.AllowGet);
+			   System.Diagnostics.Debug.WriteLine("CLIENT INDEX FUNCTION EXECUTED!!!!!!!!!@@@@@@");
 
 
-        }
+			   var clientDetails1 = from s in db.Clients
+									select new Client
+									{
+										ClientId = s.ClientId,
+										ClientName = s.ClientName,
+										ClientSubbusiness = s.ClientSubbusiness,
+										LastModifiedUserId = s.LastModifiedUserId,
+										LastModified = s.LastModified,
+										Active = s.Active,
 
-        // [HttpPost]
-        // [ValidateAntiForgeryToken]
-        // public Nullable<int> LastModifiedUserId { get; set; }
-        /*  public ActionResult Create([n(Include = "ClientId, ClientName, ClientSubbusiness,LastModifiedUser, LastModified, Active")] Client client {
+									};
+			   return Json(clientDetails1, JsonRequestBehavior.AllowGet);
+
+		   } */
+
+		// CLIENT INFO BY ID
+		//[HttpGet]
+		//public ActionResult ClientDetails(int? id)
+		//{
+
+
+
+		//    if (id == null)
+		//    {
+		//        return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+		//    }
+		//    Client client = db.Clients.Find(id);
+		//    if (client == null)
+		//    {
+		//        return HttpNotFound();
+		//    }
+		//    return Json(client, JsonRequestBehavior.AllowGet);
+		//}
+
+		/*  public ActionResult Create()
+		  {
+			  return View();
+		  } */
+
+		// [HttpPost]
+		// [ValidateAntiForgeryToken]
+		// public Nullable<int> LastModifiedUserId { get; set; }
+		/*  public ActionResult Create([n(Include = "ClientId, ClientName, ClientSubbusiness,LastModifiedUser, LastModified, Active")] Client client {
 
               if (ModelState.IsValid) {
 
@@ -91,6 +101,6 @@ namespace SogetiStaffingPlanner.Controllers
               }
 
           }*/
-    }
+	}
 
 }

c8f06f9004032d48a0be2c5175f44e7cfeb30613
mdng223 <mdng223@g.uky.edu>
2018-06-25 15:31:21 -0400

    added index files for all the views

---
+++ SogetiStaffingPlanner/Controllers/AccountExecutiveController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SogetiStaffingPlanner.Controllers
+{
+    public class AccountExecutiveController : Controller
+    {
+        // GET: AccountExecutive
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

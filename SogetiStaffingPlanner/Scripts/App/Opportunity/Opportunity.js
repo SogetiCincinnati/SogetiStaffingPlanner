3749571a640c5e3df41127b887060f49e6681e93
mdng223 <mdng223@g.uky.edu>
2018-06-25 18:20:49 -0400

    added form validation for opportunity. Need to validate ID's.

--- SogetiStaffingPlanner/Scripts/App/Opportunity/Opportunity.js
+++ SogetiStaffingPlanner/Scripts/App/Opportunity/Opportunity.js
@@ -11,11 +11,74 @@
         unitId: null,
         regionId: null,
         soldStatusId: null,
-        opportunityOwnerUserId: null
+        opportunityOwnerUserId: null,
+        errors: [],
     },
     methods: {
         onSubmit: function () {
             let data = {};
+            this.getForm(data);
+            this.checkForm();
+            if (!this.errors.length) {
+                $.ajax({
+                    type: "POST",
+                    url: "AddOpportunity",
+                    dataType: "json",
+                    data: JSON.stringify(data),
+                    contentType: "application/json; charset=utf-8",
+                    success: function (res) {
+                        alert("Added " + this.opportunityName + "!");
+                        this.opportunities.push(data);
+                        this.clearForm();
+                    }.bind(this),
+                    error: function (e) {
+                        console.log(e);
+                        console.log(e, "Error adding data! Please try again.");
+                    }
+                });
+            }
+ 
+        },
+        checkForm: function () { 
+            this.errors = [];
+
+            /*Checks to see if forms are empty */
+            if (!this.opportunityName) {
+                this.errors.push('Opportunity Name required.');
+            } if (!this.clientId) {
+                this.errors.push('Client ID required.');
+            } if (!this.clientContact) {
+                this.errors.push('Client Contact required.');
+            } if (!this.opportunityNotes) {
+                this.errors.push('Opportunity Notes required.');
+            } if (!this.accountExecutiveUserId) {
+                this.errors.push('Account Executive User ID required.');
+            } if (!this.clientId) {
+                this.errors.push('Client ID required.');
+            } if (!this.regionId) {
+                this.errors.push('Region ID required.');
+            } if (!this.soldStatusId) {
+                this.errors.push('Sold Status ID required.');
+            } if (!this.opportunityOwnerUserId) {
+                this.errors.push('Opportunity Owner User ID required.');
+            }
+            console.log('opportunityname: ' + typeof (this.opportunityName)); console.log('clientId: ' + typeof (this.clientId));
+            /* Looks for duplicate Opportunity Names */
+            for (let i = 0; i < this.opportunities.length; i++) {
+                if (this.opportunityName == this.opportunities[i]['opportunityName']) {
+                    this.errors.push('Opportunity Name: "' + this.opportunityName + '" already exists.')
+                    break;
+                }
+            }
+
+            if (!this.errors.length) { return true; }
+        },
+        cancel: function () {
+            this.errors = [];
+            this.addState = false;
+        },
+        getForm: function (data) {
+            console.log(this.opportunityName);
             data.opportunityName = this.opportunityName;
             data.opportunityNotes = this.opportunityNotes;
             data.clientContact = this.clientContact;
@@ -26,36 +89,19 @@
             data.soldStatusId = this.soldStatusId;
             data.opportunityOwnerUserId = this.opportunityOwnerUserId;
             data.active = true;
-            console.log(data);
-            $.ajax({
-                type: "POST",
-                url: "AddOpportunity",
-                dataType: "json",
-                data: JSON.stringify(data),
-                contentType: "application/json; charset=utf-8",
-                success: function (res) {
-                    alert("Added " + this.opportunityName + "!");
-                    console.log('POST request success');
-                    console.log('data', data);
-                    this.opportunities.push(data);
-                    console.log(this.opportunities);
-                    this.addState = false;
-                    this.opportunityName = '';
-                    this.opportunityNotes = '';
-                    this.clientContact = '';
-                    this.clientID = null;
-                    this.accountExecutiveUserId = null;
-                    this.unitId = null;
-                    this.regionId = null;
-                    this.soldStatusId = null;
-                    this.opportunityOwnerUserId = null;
-                    this.clientId = null;
-                }.bind(this),
-                error: function (e) {
-                    console.log(e);
-                    console.log(e, "Error adding data! Please try again.");
-                }
-            });
+        },
+        clearForm: function () {
+            this.addState = false;
+            this.opportunityName = '';
+            this.opportunityNotes = '';
+            this.clientContact = '';
+            this.clientID = null;
+            this.accountExecutiveUserId = null;
+            this.unitId = null;
+            this.regionId = null;
+            this.soldStatusId = null;
+            this.opportunityOwnerUserId = null;
+            this.clientId = null;
         },
         onEdit: function (name) {
             alert(name);
@@ -71,7 +117,6 @@
             dataType: "json",
             success: function (data) {
                 this.opportunities = data;
-                console.log(this.opportunities);
             }.bind(this)
         });
     }

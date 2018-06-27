dee19d93eb7914e2e50dd4c37ab78faad7f4f6ab
Zoe Baker <Zoe.Baker@us.sogeti.com>
2018-06-27 12:25:35 -0400

    169-E-Edit Client - Update Client Data Access - Added the edit client method in the client controller and added ClientData.cs to hold extra information for client to send to front end in the getClients method so username is included.

---
+++ SogetiStaffingPlanner/Models/ClientData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SogetiStaffingPlanner.Models
+{
+	public class ClientData
+	{
+		public int ClientId { get; set; }
+		public string ClientName { get; set; }
+		public string ClientSubbusiness { get; set; }
+		public int LastModifiedUserId { get; set; }
+		public string LastModifiedUserName {get; set;}
+		public DateTime LastModified { get; set; }
+		public bool Active { get; set; }
+	}
+}

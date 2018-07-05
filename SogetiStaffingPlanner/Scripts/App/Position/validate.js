b7c3ba639f7e90c49b40ec928c3a23547a7a8cd8
mdng223 <mdng223@g.uky.edu>
2018-07-05 14:50:08 -0400

    Checks for decimal numbers for 'rate', 'number of positions', and 'duration' in Position view.

--- SogetiStaffingPlanner/Scripts/App/Position/validate.js
+++ SogetiStaffingPlanner/Scripts/App/Position/validate.js
@@ -28,18 +28,25 @@
         if (val < 1) {
             that.errors.duration = 'Duration has to be atleast 1';
         } else { that.errors.duration = ''; }
+        if (val % 1 != 0) {
+            that.errors.duration += ' Cannot be a decimal.'
+        }
     },
     checkRate: function (val, that) {
         if (val < 30) {
-            that.errors.rate = 'Rate cannot be below 30';
+            that.errors.rate = 'Rate cannot be below 30.';
         } else { that.errors.rate = ''; }
+        if (val % 1 != 0) { // check for decimals
+            that.errors.rate += ' Cannot be a decimal.'
+        }
     },
     checkNumberOfPositions: function (val, that) {
-        try {
-            if (val.length || val) { that.errors.numberOfPositions = ''; }
-            else { that.errors.numberOfPositions = 'Number of Positions required'; }
-        } catch (e) { }
-
+        if (val < 1) {
+            that.errors.numberOfPositions = 'Number of Positions has to be atleast 1.';
+        } else { that.errors.numberOfPositions = ''; }
+        if (val % 1 != 0) {
+            that.errors.numberOfPositions += ' Cannot be a decimal.'
+        }
     },
     checkPositionStatusId: function (val, that) {
         if (val) {

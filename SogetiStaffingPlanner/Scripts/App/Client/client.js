d80d45527f8a01514035b119d692391fd191d1cf
mdng223 <mdng223@g.uky.edu>
2018-07-05 11:15:29 -0400

    fixed validation for clients

--- SogetiStaffingPlanner/Scripts/App/Client/client.js
+++ SogetiStaffingPlanner/Scripts/App/Client/client.js
@@ -1,35 +1,98 @@
 ﻿new Vue({
     el: '#createClient',
     data: {
-        clients: '',
-        clientName: '',
-        clientSubbusiness: '',
-        addState: false,
+        clients: [],
+        formData: {
+            clientName: '',
+            clientSubbusiness: '',
+        },
+        states: {
+            addState: false,
+            updateState: false,
+        },
+        errors: {
+            clientName: null,
+            clientSubbusiness: null
+        }
+    },
+    computed: {
+        computeClientName() {
+            return this.formData.clientName;
+        },
+        computeClientSubbusiness() {
+            return this.formData.clientSubbusiness;
+        }
+    },
+    watch: {
+        computeClientName: function (val) {
+            try {
+                if (val || val.length) { this.errors.clientName = ''; }
+                else { this.errors.clientName = 'Client name required'; }
+            } catch (e) { }
+
+
+            if (!this.states.updateState) {
+                for (let i = 0; i < this.clients.length; i++) {
+                    if (val == this.clients[i].ClientName) {
+                        this.errors.clientName = '"' + this.clients[i].ClientName + '" already exists.';
+                        break;
+                    }
+                }
+            }
+        },
+        computeClientSubbusiness: function (val) {
+            try {
+                if (val || val.length) { this.errors.clientSubbusiness = ''; }
+                else { this.errors.clientSubbusiness = 'Client Subbusiness required'; }
+            } catch (e) { }
+
+        }
     },
     methods: {
         onSubmit: function () {
-            let data = {};
-            data.clientName = this.clientName;
-            data.clientSubbusiness = this.clientSubbusiness;
-            console.log(data);
             $.ajax({
                 type: "POST",
                 url: "AddClient",
                 dataType: "json",
-                data: JSON.stringify(data),
+                data: JSON.stringify(this.formData),
                 contentType: "application/json; charset=utf-8",
                 success: function (res) {
                     //Receives message from backend for you to do what you want with it
                     console.log('POST request success');
-                    alert('Client Name: "' + this.clientName + '" and Client Subbusiness: "' + this.clientSubbusiness + '" successfully added.');
-                    this.clientName = '';
-                    this.clientSubbusiness = '';
-                    this.clients.push(data);
+                    alert('Client Name: "' + this.formData.clientName + '" and Client Subbusiness: "' + this.formData.clientSubbusiness + '" successfully added.');
+                    this.formData = [];
+                    this.states.addState = false;
+                    $.ajax({
+                        async: false,
+                        cache: false,
+                        type: "GET",
+                        url: "GetClients",
+                        contentType: "application/json;charset=utf-8",
+                        dataType: "json",
+                        success: function (data) {
+                            this.clients = data;
+                        }.bind(this)
+                    });
                 }.bind(this),
                 error: function (e) {
                     console.log(e, "Error adding data! Please try again.");
                 }
             });
+        },
+        onEdit: function (client) {
+            this.errors = {};
+            console.log(client);
+            this.states.addState = true;
+            this.states.updateState = true;
+            this.formData.clientName = client.ClientName;
+            this.formData.clientSubbusiness = client.ClientSubbusiness;
+            window.scrollTo(0, 100);
+        },
+        cancel: function () {
+            this.states.addState = false;
+            this.states.updateState = false;
+            this.errors = {};
+            this.formData = [];
         }
     },
     created: function () {

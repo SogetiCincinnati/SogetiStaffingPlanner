b5febad5fc3c93e492f4aebafca6b188132e47c1
mdng223 <mdng223@g.uky.edu>
2018-07-06 15:06:42 -0400

    added Users. It displays the data for users now.

---
+++ SogetiStaffingPlanner/Models/UserData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SogetiStaffingPlanner.Models
+{
+	/*
+	* Used to store data from the Client Model + extra values for 
+	* displaying foreign keys to the user in an understandable way. 
+	*/
+	public class UserData
+	{
+		//Primary Key
+		public string FullName { get; set; }
+
+		//Foreign Keys and their corresponding display values
+		public int LastModifiedUserId { get; set; }
+		public string LastModifiedUserName { get; set; }
+        public int ViewRoleId { get; set; }
+        public int PermissionRoleId { get; set; }
+
+        //Remaining Client Fields
+		public bool Active { get; set; }
+        public DateTime lastModified { get; set; }
+    }
+}

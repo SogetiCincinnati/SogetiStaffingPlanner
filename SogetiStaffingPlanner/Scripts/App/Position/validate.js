54defb6977172fe0eb570069642ec69b2939bfd6
Travis Turner <travis@travisturner.io>
2018-07-06 09:36:32 -0400

    Fix an issue where validation error was giving a value of 'undefined' if number was a negative fraction.

--- SogetiStaffingPlanner/Scripts/App/Position/validate.js
+++ SogetiStaffingPlanner/Scripts/App/Position/validate.js
@@ -14,13 +14,13 @@
         }
         try {
             if (that.duration) {
-                if (that.duration < 1) {
-                    that.errors.duration = 'Duration has to be atleast 1';
-                } 
-                if (that.duration % 1 != 0) {
-                    that.errors.duration += ' Cannot be a decimal.'
+                if (that.duration < 1 || that.duration == "" || typeof that.duration == "string") {
+                    that.errors.duration = 'Duration must be 1 or above.';
                 }
-            }
+                if (that.duration % 1 != 0) {
+                    that.errors.duration ? that.errors.duration += ' Cannot be a decimal.' : that.errors.duration = 'Cannot be a decimal.';
+                }
+            }   
 
         } catch (e) { }
         try {
@@ -29,17 +29,17 @@
                     that.errors.rate = 'Rate cannot be below 30.';
                 } 
                 if (that.rate % 1 != 0) { // check for decimals
-                    that.errors.rate += ' Cannot be a decimal.'
+                    that.errors.rate ? that.errors.rate += ' Cannot be a decimal.' : that.errors.rate = 'Cannot be a decimal.'; 
                 }
             }
         } catch (e) { }
         try {
             if (that.numberOfPositions) {
                 if (that.numberOfPositions < 1) {
-                    that.errors.numberOfPositions = 'Number of Positions has to be atleast 1.';
+                    that.errors.numberOfPositions = 'Number of Positions must be greater than 1.';
                 }
                 if (that.numberOfPositions % 1 != 0) {
-                    that.errors.numberOfPositions += ' Cannot be a decimal.'
+                    that.errors.numberOfPositions ? that.errors.numberOfPositions += ' Cannot be a decimal.' : that.errors.numberOfPositions = 'Cannot be a decimal.'; 
                 }
             }
 
@@ -62,7 +62,7 @@
     checkDuration: function (val, that) {
         try {
             if (val < 1) {
-                that.errors.duration = 'Duration has to be atleast 1';
+                that.errors.duration = 'Duration must be 1 or above.';
             } else { that.errors.duration = ''; }
             if (val % 1 != 0) {
                 that.errors.duration += ' Cannot be a decimal.'
@@ -84,7 +84,7 @@
     checkNumberOfPositions: function (val, that) {
         try {
             if (val < 1) {
-                that.errors.numberOfPositions = 'Number of Positions has to be atleast 1.';
+                that.errors.numberOfPositions = 'Number of Positions must be greater than 1.';
             } else { that.errors.numberOfPositions = ''; }
             if (val && val % 1 != 0) {
                 that.errors.numberOfPositions += ' Cannot be a decimal.'

d1275597c21f9215db5aa82106629ffe7b8ebe29
Travis Turner <travis@travisturner.io>
2018-07-06 09:10:49 -0400

    72-A-Update controllers to handle new DB field required standards - Fix 'GetMainData' so that nullable values aren't conflicting with current table population.

--- SogetiStaffingPlanner/Controllers/HomeController.cs
+++ SogetiStaffingPlanner/Controllers/HomeController.cs
@@ -61,30 +61,41 @@ namespace SogetiStaffingPlanner.Controllers
                         OpportunityName = mvR.OpportunityName,
                         AEName = mvR.AEName,
                         OpActive = mvR.OpActive,
+                        
                         ClientContact = mvR.ClientContact,
+                        
                         ClientName = mvR.ClientName,
+                        
                         ClientSubbusiness = mvR.ClientSubbusiness,
-                        MaxConsultantGrade = mvR.MaxConsultantGrade,
-                        ExpectedStartDate = mvR.ExpectedStartDate,
-                        LastModified = mvR.LastModified,
+                        //MaxConsultantGrade = mvR.MaxConsultantGrade,
+                        //ExpectedStartDate = mvR.ExpectedStartDate,
+                        //LastModified = mvR.LastModified,
+                        
                         NumberOfPositions = mvR.NumberOfPositions,
+                        /*
                         PracticeName = mvR.PracticeName,
                         ProposedCandidate = mvR.ProposedCandidate,
                         Rate = mvR.Rate,
                         Skillset = mvR.Skillset,
                         AcceptedCandidate = mvR.AcceptedCandidate,
+                        */
                         ACTName = mvR.ACTName,
+                        /*
                         Duration = mvR.Duration,
                         HireCandidate = mvR.HireCandidate,
                         MinConsultantGrade = mvR.MinConsultantGrade,
                         PositionStatusName = mvR.PositionStatusName,
                         PositionNote = mvR.PositionNote,
                         RejectedCandidate = mvR.RejectedCandidate,
-                        SActive = mvR.SActive,
+                        SActive = mvR.SActive,*/
                         UnitName = mvR.UnitName,
+                        
                         Priority = CalculatePriority(mvR),
+                        
+                        /*
                         ExpectedStartDateString = Convert.ToString(mvR.ExpectedStartDate.Value.Month) + "/" + Convert.ToString(mvR.ExpectedStartDate.Value.Day) + "/" + Convert.ToString(mvR.ExpectedStartDate.Value.Year),
                         LastModifiedString = Convert.ToString(mvR.LastModified.Value.Month) + "/" + Convert.ToString(mvR.LastModified.Value.Day) + "/" + Convert.ToString(mvR.LastModified.Value.Year),
+                        */
                         SoldStatusName = mvR.SoldStatusName,
                         PositionName = mvR.PositionName 
                     });

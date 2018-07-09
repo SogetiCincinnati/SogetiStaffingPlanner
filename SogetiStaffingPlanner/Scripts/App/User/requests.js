6d91a4c589c6fef90e08f0840c999d92f413f770
Travis Turner <travis@travisturner.io>
2018-07-09 10:45:09 -0400

    Refactor requests code into seperate .js file.

---
+++ SogetiStaffingPlanner/Scripts/App/User/requests.js
@@ -0,0 +1,52 @@
+﻿let requests = {
+    fetchUsers: function (that) {
+        $.ajax({
+            async: false,
+            cache: false,
+            type: "GET",
+            url: "GetUsers",
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                that.users = data;
+            }.bind(that)
+        });
+    },
+    addUser: function (that, data) {
+        $.ajax({
+            async: false,
+            cache: false,
+            type: "POST",
+            url: "AddUser",
+            dataType: "json",
+            data: JSON.stringify(data),
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                requests.fetchUsers(that);
+                that.formData = {};
+                that.states.addState = false;
+            }.bind(that)
+        });
+    },
+    editUser: function (that, data) {
+        console.log('from requests: ',data);
+        $.ajax({
+                type: "POST",
+                url: "EditUser",
+                dataType: "json",
+                data: JSON.stringify(data),
+                contentType: "application/json; charset=utf-8",
+                success: function (res) {
+                    //Receives message from backend for you to do what you want with it
+                    console.log('POST request success');
+                    that.states.addState = false;
+                    that.states.updateState = false;
+                    requests.fetchUsers(that);
+                }.bind(that),
+                error: function (e) {
+                    console.log(e, "Error adding data! Please try again.");
+                }
+            });
+    }
+}
--- SogetiStaffingPlanner/Scripts/App/User/user.js
+++ SogetiStaffingPlanner/Scripts/App/User/user.js
@@ -22,20 +22,13 @@
             this.states.addState = true;
             window.scrollTo(0, 100);
         },
-        onSubmit: function () {
-            let data = {};
-            data.fullName = this.formData.name;
-            data.active = true;
-            data.lastModifiedUserId = 1;
-            data.viewRoleId = 1;
-            data.permissionRoleId = 1;
-            data.lastModified = new Date();
+        onSubmit: function () {      
             if (this.states.updateState) {
                 console.log('update called.')      
                 this.updateUser();
             } else if (this.states.addState) {
                 console.log('add called');
-                this.addUser(data);
+                this.addUser();
             } else {
                 console.log('Something unexpected happened.');
             }
@@ -58,78 +51,30 @@
             window.scrollTo(0, 100);
         },
         updateUser: function () {
-            $.ajax({
-                type: "POST",
-                url: "EditUser",
-                dataType: "json",
-                data: JSON.stringify(this.formData),
-                contentType: "application/json; charset=utf-8",
-                success: function (res) {
-                    //Receives message from backend for you to do what you want with it
-                    console.log('POST request success');
-                    this.states.addState = false;
-                    this.states.updateState = false;
-                    $.ajax({
-                        async: false,
-                        cache: false,
-                        type: "GET",
-                        url: "GetUsers",
-                        contentType: "application/json;charset=utf-8",
-                        dataType: "json",
-                        success: function (data) {
-                            this.users = data;
-                        }.bind(this)
-                    });
-                }.bind(this),
-                error: function (e) {
-                    console.log(e, "Error adding data! Please try again.");
-                }
-            });
+            let data = {};
+            data.name = this.formData.name;
+            data.permission = this.formData.permission;
+            data.userId = this.formData.userId;
+            data.role = this.formData.role;
+            data.active = true;
+            data.lastModifiedUserId = 1;
+            data.viewRoleId = 1;
+            data.permissionRoleId = 1;
+            data.lastModified = new Date();
+            requests.editUser(this, data);
         },
-        addUser: function (data) {
-            $.ajax({
-                async: false,
-                cache: false,
-                type: "POST",
-                url: "AddUser",
-                dataType: "json",
-                data: JSON.stringify(data),
-                contentType: "application/json;charset=utf-8",
-                dataType: "json",
-                success: function (response) {
-                    $.ajax({
-                        async: false,
-                        cache: false,
-                        type: "GET",
-                        url: "GetUsers",
-                        contentType: "application/json;charset=utf-8",
-                        dataType: "json",
-                        success: function (data) {
-                            alert("User added!");
-                            this.formData = {};
-                            this.users = data;
-                            this.states.addState = false;
-                        }.bind(this)
-                    });
-                    console.log(response);
-                    console.log("On Submit Called");
-                }.bind(this)
-            });
-        }
-        
+        addUser: function () {
+            let data = {};
+            data.fullName = this.formData.name;
+            data.active = true;
+            data.lastModifiedUserId = 1;
+            data.viewRoleId = 1;
+            data.permissionRoleId = 1;
+            data.lastModified = new Date();
+            requests.addUser(this, data);
+        }    
     },
     created() {
-        $.ajax({
-            async: false,
-            cache: false,
-            type: "GET",
-            url: "GetUsers",
-            contentType: "application/json;charset=utf-8",
-            dataType: "json",
-            success: function (data) {
-                this.users = data;
-            }.bind(this)
-        });
+        requests.fetchUsers(this);
     }
-    
 });
